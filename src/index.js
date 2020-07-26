ae2ac2a90c05083a1d1f60b051a77bfef64d0aa5
Okubo <aspastrana990@gmail.com>
2020-07-26 01:50:45 -0300

    React Template first Commit

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from "react"
+import reactDOM from "react-dom"
+
+const template = <h1>Webpack - React Initial Template</h1>
+
+reactDOM.render(template, document.getElementById("root"))
